df2c90fd14c9e06e499d8d0667b31fabae2899f3
novidd <clm.pettersson@gmail.com>
2025-09-25 17:48:22 +0200

    autofocus added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -49,6 +49,7 @@ export const Editor = ({ initialContent }: EditorProps) => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
+    autofocus: true,
     onCreate({ editor }) {
       setEditor(editor);
     },
@@ -76,8 +77,7 @@ export const Editor = ({ initialContent }: EditorProps) => {
     editorProps: {
       attributes: {
         style: `padding-left: ${leftMargin}px; padding-right: ${rightMargin}px;`,
-        class:
-          `focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text`,
+        class: `focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text`,
       },
     },
     extensions: [
@@ -116,7 +116,9 @@ export const Editor = ({ initialContent }: EditorProps) => {
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
       <Ruler />
-      <div className={`min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0`}>
+      <div
+        className={`min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0`}
+      >
         <EditorContent editor={editor} />
         <Threads editor={editor} />
       </div>
